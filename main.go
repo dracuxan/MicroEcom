26b859ee52a63ee53dd748032bb52fa53bcc2bcc
dracuxan <khodkenisarg@gmail.com>
2025-06-01 00:03:36 +0530

    finish v1

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+func greet(w http.ResponseWriter, r *http.Request) {
+	d, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "fucl", http.StatusBadRequest)
+		return
+	}
+
+	log.Printf("Data received: %s\n", d)
+	fmt.Fprintf(w, "Hello %s!\n", d)
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	http.HandleFunc("/", greet)
+	http.ListenAndServe(":9090", nil)
 }
